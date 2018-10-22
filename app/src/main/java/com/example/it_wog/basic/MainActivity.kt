583e48f5b8b1f4559e5b700300da345bc1720619
Wolfgang <wolfgang.eberding@googlemail.com>
2018-10-22 11:01:43 +0200

    more about string resources and translation in other languages

--- app/src/main/java/com/example/it_wog/basic/MainActivity.kt
+++ app/src/main/java/com/example/it_wog/basic/MainActivity.kt
@@ -5,8 +5,10 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import com.example.it_wog.basic.R.id.button
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +21,11 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        // res == R; strings == string
+        val text = getString(R.string.mainActivityWelcomeMsg)
+        //with getString I can use defined resources
+        val text2 = getString(R.string.profAlter)
+        button.text = text
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
